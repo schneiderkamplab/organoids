55e8da8f3d564b6a16e1a92d86f7f816de3a7581
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-03-13 19:32:52 +0100

    absolute size, rank, largest

--- organoids/commands/rank.py
+++ organoids/commands/rank.py
@@ -1,7 +1,6 @@
 import os
 import json
 import click
-import exif
 import tqdm
 import numpy as np
 import shapely
@@ -17,32 +16,64 @@ def load_json(file_path):
         return json.load(f)
 
 @_rank.command()
-@click.argument("dir", type=click.Path(exists=True))
-def rank(dir):
+@click.argument("directory", type=click.Path(exists=True))
+@click.argument("output", type=click.Path())
+@click.option("--ext", default=".json", help="File extension to search for (default: .json)")
+@click.option("--separator", default="\t", help="Separator for CSV file (default: tab).")
+@click.option("--id-separator", default="-kopi", help="Separator for id in file name (default: -kopi).")
+@click.option("--decimal-separator", default=".", help="Decimal separator for CSV file (default: .).")
+def rank(directory, output, ext, separator, id_separator, decimal_separator):
+    start("Scanning for files")
+    todo = [directory]
+    found = []
+    while todo:
+        dir = todo.pop()
+        for entry in os.listdir(dir):
+            entry_path = os.path.join(dir, entry)
+            if os.path.isdir(entry_path):
+                todo.append(entry_path)
+            elif entry.endswith(ext):
+                found.append(entry_path)
+    status(len(found), end='')
+    end()
     start("Computing rank")
-    todo = [f for f in os.listdir(dir) if f.endswith(".json")]
-    
-    for file in tqdm.tqdm(todo):
-        data = load_json(os.path.join(dir, file))
-        if "shapes" not in data.keys():
-            print(f"Warning: {file} has no shapes data")
-        
-        poly_data = []
-        shapes = data["shapes"]
-        for shape in shapes:
-            if 'label' not in shape.keys():
-                print(f"Warning: {file} contains shapes without label-key")
-            
-            label = shape['label']
-            poly = shapely.geometry.Polygon(shape['points'])
-            area = poly.area
-            poly_data.append({'label': label, 'area:': area})
+    with open(output, "w") as f:
+        f.write(f"id{separator}{separator.join([f'area{i+1}' for i in range(12)])}{separator}{separator.join([f'rank{i+1}' for i in range(12)])}{separator}{separator.join([f'largest{i+1}' for i in range(12)])}\n")
+        for file in tqdm.tqdm(found):
+            data = load_json(file)
+            if "shapes" not in data.keys():
+                print(f"Warning: {file} has no shapes data")
+                continue
+            if not "kopi" in file:
+                print(f"Warning: {file} does not have the expected name format ID{id_separator}.json")
+                continue
+            poly_data = []
+            shapes = data["shapes"]
+            for shape in shapes:
+                if 'label' not in shape.keys():
+                    print(f"Warning: {file} contains shapes without label-key")
+                    continue
+                label = shape['label'].replace("?", "")
+                poly = shapely.geometry.Polygon(shape['points'])
+                area = poly.area
+                poly_data.append({'label': label, 'area:': area})
 
-        # sort polygons by area in decreasing order 
-        poly_data.sort(key=lambda x: x['area:'], reverse=True)
-        sorted_labels = [poly['label'] for poly in poly_data]    
-    
-        start("Writing areas to disk")
-        with open(os.path.join(dir, f"{file}.csv"), "w") as f:
-            f.write(','.join(sorted_labels))
-        end()
+            # id from file name
+            id = file.split("/")[-1].split(id_separator)[0]
+
+            # list areas
+            label2area = {int(poly['label']): poly['area:'] for poly in poly_data}
+            areas = separator.join(str(label2area.get(i+1)).replace('.',decimal_separator) for i in range(12))
+
+            # sort polygons by area in decreasing order
+            poly_data.sort(key=lambda x: x['area:'], reverse=True)
+            sorted_labels = [poly['label'] for poly in poly_data]
+            while len(sorted_labels) < 12:
+                sorted_labels.append('')
+            largest = separator.join(sorted_labels)
+
+            # rank polygons by area
+            ranks = separator.join((str(sorted_labels.index(str(i+1))+1) if str(i+1) in sorted_labels else '') for i in range(12))
+
+            # write to CSV file
+            f.write(f'"{id}"{separator}{areas}{separator}{ranks}{separator}{largest}\n')
